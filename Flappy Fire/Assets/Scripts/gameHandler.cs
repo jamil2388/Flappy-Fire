baa0a8882c3418d5f45f1cd876a83d6cf65d10b9
jamil2388 <manujamil88@gmail.com>
2018-01-17 22:41:19 +0600

    touch prototype 1

---
+++ Flappy Fire/Assets/Scripts/gameHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameHandler : MonoBehaviour
+{
+	//public vars
+	
+
+	//private vars
+	//to enable monitoring touch input
+	private bool handleTouch;
+	//to store the touch value
+	private Touch touch;
+
+	// Use this for initialization
+	void Start ()
+	{
+		handleTouch = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (handleTouch) {
+			executeTouch ();
+		}
+	}
+
+	//to handle the touch activities
+	void executeTouch ()
+	{
+		if (Input.touchCount > 0) {
+			touch = Input.GetTouch (0);
+			if (touch.phase == TouchPhase.Began) {
+				print ("the player should jump here");
+			}
+		}
+	}
+}
